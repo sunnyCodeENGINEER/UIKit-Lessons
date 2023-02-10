15f08448ffe21047de89622b8df9dfe41d8b7c8e
Emmanuel Donkor <sunnycodeengineer@gmail.com>
2023-02-10 13:28:30 +0000

    Revision on first lesson.

--- UIKitFirstLesson/SecondScreen.swift
+++ UIKitFirstLesson/SecondScreen.swift
@@ -9,15 +9,43 @@ import UIKit
 
 class SecondScreen: UIViewController {
 
+    let nextButton = UIButton()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Second Screen"
+        setUpBotton()
         navigationController?.navigationBar.prefersLargeTitles = true
         view.backgroundColor = .systemMint
 
+        
         // Do any additional setup after loading the view.
     }
     
+    func setUpBotton() {
+        view.addSubview(nextButton)
+        
+        nextButton.configuration = .filled()
+        nextButton.configuration?.title = "Next"
+        nextButton.configuration?.baseBackgroundColor = .systemPink
+        
+        nextButton.addTarget(self, action: #selector(goToNextScreen), for: .touchUpInside)
+        
+        nextButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            nextButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            nextButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            nextButton.widthAnchor.constraint(equalToConstant: 200),
+            nextButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
+        
+    }
+    
+    @objc func goToNextScreen() {
+        let nextScreen = ThirdScreen()
+        navigationController?.pushViewController(nextScreen, animated: true)
+    }
 
     /*
     // MARK: - Navigation
---
+++ UIKitFirstLesson/ThirdScreen.swift
@@ -0,0 +1,58 @@
+//
+//  ThirdScreen.swift
+//  UIKitFirstLesson
+//
+//  Created by Emmanuel Donkor on 10/02/2023.
+//
+
+import UIKit
+
+class ThirdScreen: UIViewController {
+
+    let homeButton = UIButton()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemCyan
+        setUpButton()
+        title = "Third Screen"
+        navigationController?.navigationBar.prefersLargeTitles = true
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func setUpButton() {
+        view.addSubview(homeButton)
+        
+        homeButton.configuration = .filled()
+        homeButton.configuration?.title = "Go To Home"
+        homeButton.configuration?.baseBackgroundColor = .green
+        
+        homeButton.addTarget(self, action: #selector(goHome), for: .touchUpInside)
+        
+        homeButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            homeButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            homeButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            homeButton.widthAnchor.constraint(equalToConstant: 250),
+            homeButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
+        
+    }
+    
+    @objc func goHome() {
+        navigationController?.popToRootViewController(animated: true)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
